af86c678db7d6bb4ac798a31e7f6b6b2d0cb4e15
Saghm Rossi <saghmrossi@gmail.com>
2015-05-29 11:35:36 -0400

    cleaned up code a bit and added second test

--- tests/lib.rs
+++ tests/lib.rs
@@ -4,12 +4,15 @@ extern crate mongodb;
 use libc::size_t;
 
 #[test]
-fn it_works() {
-    let doc = mongodb::get_sample_bson_doc();
-    unsafe {
-        assert_eq!((*doc).len, 14);
-    }
+fn single_key_document() {
+    let doc = mongodb::get_single_key_bson_doc();
     let s = mongodb::bson_to_json(doc);
     assert_eq!(s, "{ \"foo\" : 42 }");
-    assert!(true);
+}
+
+#[test]
+fn multi_key_document() {
+    let doc = mongodb::get_multi_key_bson_doc();
+    let s = mongodb::bson_to_json(doc);
+    assert_eq!(s, "{ \"foo\" : 42, \"bar\" : \"shallow\" }");
 }
